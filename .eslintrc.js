de2803c1f8b35836edbae14560bb0c6cdd58d7a9
geek-tim <t13691800512@163.com>
2021-11-04 18:04:41 +0800

    chore: 增加编码规范与提交规范校验

---
+++ .eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+    es2020: true,
+    jest: true
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ],
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off', // 允许使用@ts-ignore
+    '@typescript-eslint/ban-types': 'off'
+    // '@typescript-eslint/no-unused-vars': 'off' // 是否检查未使用变量
+  },
+  overrides: [
+    {
+      // enable the rule specifically for TypeScript files
+      files: ['*.ts', '*.tsx']
+      // rules: {
+      //   '@typescript-eslint/explicit-module-boundary-types': 'warn'
+      // }
+    }
+  ]
+}
